7fb5631ffd078ef813af6d574d7122098d2d7153
Tom Jane <kodemonki@hotmail.com>
2018-05-05 13:59:43 +0100

    updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,9 @@ const htmlWebpackPlugin = new HtmlWebPackPlugin({
 });
 
 module.exports = {
+  output:{
+    publicPath: '/'
+  },
   module: {
     rules: [
       {
@@ -21,6 +24,9 @@ module.exports = {
         use: ['style-loader','css-loader', 'sass-loader']
       }
     ]
+  },
+  devServer: {
+    historyApiFallback: true,
   },
   plugins: [htmlWebpackPlugin]
 };
